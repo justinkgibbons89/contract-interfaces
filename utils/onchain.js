7298bb5ef029b825cf4c4b45c576eae6afe1351a
justin <justinkgibbons@gmail.com>
2022-05-08 17:06:17 -0700

    fix gitignore, update tsconfig, fix double tests

--- utils/onchain.js
+++
@@ -1,7 +0,0 @@
-import { ethers } from "ethers";
-
-export const decodeLogEvent = (data, topics, abi) => {
-	const iface = new ethers.utils.Interface(abi);
-	const parsed = iface.parseLog({ data, topics });
-	return parsed;
-}
